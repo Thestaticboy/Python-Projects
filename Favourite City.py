ba9199f0cb99613335b776c3015040a58e9a6e50
Thestaticboy <168290972+Thestaticboy@users.noreply.github.com>
2024-05-04 12:50:15 +0100

    New
    New

---
+++ Favourite City.py
@@ -0,0 +1,8 @@
+# Prints out the name of a favourite city
+def favourite_city(name):
+    print("One of my favourite cities is", name)
+
+favourite_city("Cardiff, Wales")
+favourite_city("London, England")
+favourite_city("Belfast, Ireland")
+favourite_city("Edinburgh, Scotland")
---
+++ Guessing Game.py
@@ -0,0 +1,13 @@
+# ask user for food input
+print("What's my favourite food?")
+food = input("Enter favourite food:")
+print("favourite food is: " + food)
+
+#output based on food
+if food == "beef":
+    print("yep! So amazing!")
+else:
+    print("Yuck! That's not it!")
+
+#Final statement
+print("Thanks for Playing!")
